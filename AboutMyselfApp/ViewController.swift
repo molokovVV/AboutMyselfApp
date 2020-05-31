bb023c0a447627518e96a6b97997b4c2fabf0e25
vmolokov <vmolokov59@gmail.com>
2020-05-31 22:52:22 +0500

    Initial Commit

---
+++ AboutMyselfApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMyselfApp
+//
+//  Created by Виталик Молоков on 31.05.2020.
+//  Copyright © 2020 Виталик Молоков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
